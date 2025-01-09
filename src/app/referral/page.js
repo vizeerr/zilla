404a6a10a95acfc6678e63bbdf1ac9c1d4c85149
vivek sagar <vivek54540@gmail.com>
2025-01-09 18:02:18 +0530

    changes before the referral page v3.3

--- src/app/referral/page.js
+++ src/app/referral/page.js
@@ -3,35 +3,36 @@ import ReferalUse from '@/components/ReferalUse'
 import TopNavbar from '@/components/TopNavbar'
 import React from 'react'
 import ref1 from '@/assets/ref1.svg'
+import ref2 from '@/assets/ref2.svg'
 import Image from 'next/image'
 import ReferalBody from '@/components/ReferalBody'
 
 const page = () => {
   return ( 
-   <div className='bg-[#111111] py-[0.9vw]'>
-    <div className='pt-[0.8vw] text-center'>
-      <p className='font-[900] text-[2vw] font-montserrat text-white'>Earn Together, <span className='text-primary'>Grow Together !</span></p>
-      <p className='text-[#FFFFFF66] mt-[1vw] font-montserrat font-[800] leading-tight text-[1vw]' >Welcome to the new way of sharing — where both sides win!</p> 
-      <p className='text-[#FFFFFF66] font-montserrat font-[700] italic mt-[0.4vw] leading-tight text-[0.8vw]'>For more information, check out our <span className='text-primary text-[1vw]'>Documentation</span> </p>
+   <div className='bg-[#111111] py-[0.8vw]'>
+    <div className='sm:pt-[0.8vw] pt-[8vw] text-center'>
+      <p className='font-[900] sm:text-[2vw] text-[6vw] sm:w-full w-[60%] mx-auto font-montserrat text-white'>Earn Together, <span className='text-primary'>Grow Together !</span></p>
+      <p className='text-[#FFFFFF66] sm:w-full w-[95%] mx-auto sm:mt-[1vw] mt-[4vw] font-montserrat font-[800] leading-tight sm:text-[1vw] text-[4vw]' >Welcome to the new way of sharing — where both sides win!</p> 
+      <p className='text-[#FFFFFF66] sm:w-full w-[90%] mx-auto font-montserrat font-[700] italic sm:mt-[0.4vw] mt-[2.5vw] leading-tight sm:text-[0.8vw] text-[3.4vw]'>For more information, check out our <span className='text-primary sm:text-[1vw] text-[3.7vw]'>Documentation</span> </p>
     </div>
-    <div className='flex justify-center items-center '>
-      <div className='flex items-center'>
-        <div className='bg-[#1B1C1E] py-[1.8vw] text-center px-[1.3vw] w-[15vw] rounded-[1.8vw] shadow-[16.21px_16.21px_56.21px_0px_#0000004F]'>
-          <p className='sachabo text-white text-[1.6vw] leading-tight'><span className='text-primary'>Earn Zilla tokens</span> when someone buys through your link.</p>
+    <div className='flex justify-center sm:items-center sm:gap-0 gap-[2.9vw] sm:my-0 my-[8.9vw]'>
+      <div className='flex items-center sm:flex-row flex-col'>
+        <div className='bg-[#1B1C1E] sm:py-[1.8vw] py-[5vw] text-center sm:px-[1.3vw] px-[2.5vw] sm:w-[15vw] w-[41vw] sm:rounded-[1.8vw] rounded-[5vw] shadow-[16.21px_16.21px_56.21px_0px_#0000004F]'>
+          <p className='sachabo text-white sm:text-[1.6vw] text-[4.1vw] leading-tight'><span className='text-primary'>Earn Zilla tokens</span> when someone buys through your link.</p>
         </div>
-        <div className='w-[13.5vw]'>
+        <div className='sm:w-[13.5vw] w-[41vw]'>
             <Image src={ref1} alt='' className='w-full'/>
           </div>
       </div>
-      <div className='flex items-center '>
-      <div className='w-[13.5vw]'>
-            <Image src={ref1} alt='' className='w-full'/>
+      <div className='flex items-center sm:flex-row flex-col-reverse'>
+        <div className='sm:w-[13.5vw] w-[41vw]'>  
+              <Image src={ref2} alt='' className='w-full'/>
+            </div>
+          <div className='bg-[#1B1C1E] sm:py-[1.8vw] py-[5vw] text-center sm:px-[1.3vw] px-[2.5vw] sm:w-[15vw] w-[41vw] sm:rounded-[1.8vw] rounded-[5vw] shadow-[16.21px_16.21px_56.21px_0px_#0000004F]'>
+            <p className='sachabo text-white sm:text-[1.6vw] text-[4.1vw] leading-tight'><span className='text-primary'>Your referral gets rewarded </span> with Zilla tokens for their purchases.</p>
           </div>
-        <div className='bg-[#1B1C1E] py-[1.8vw] text-center px-[1.3vw] w-[15vw] rounded-[1.8vw] shadow-[16.21px_16.21px_56.21px_0px_#0000004F]'>
-          <p className='sachabo text-white text-[1.6vw] leading-tight'><span className='text-primary'>Earn Zilla tokens</span> when someone buys through your link.</p>
-        </div>
         
-      </div>
+        </div>
     </div>
     <ReferalBody/>
     
--- src/components/ReferalBody.jsx
+++ src/components/ReferalBody.jsx
@@ -2,10 +2,13 @@
 import React, { useEffect, useState } from 'react'
 import info from '@/assets/info.svg'
 import Image from 'next/image';
+import { motion } from 'framer-motion';
+
 const ReferalBody = () => {
   const [targetDay, setTargetDay] = useState('2025-01-15'); // Set default target date
   const [timeLeft, setTimeLeft] = useState({});
-
+  const [hoveredCard, setHoveredCard] = useState(null);
+  
   const calculateTimeLeft = () => {
     const now = new Date();
     const targetDate = new Date(targetDay);
@@ -40,72 +43,103 @@ const ReferalBody = () => {
     setTargetDay(e.target.value);
   };
   return (
-    <div className='flex justify-center gap-[3.9vw] mt-[1vw]'>
-      <div className='bg-[#1B1C1E] rounded-[2.5vw] shadow-[16.21px_16.21px_56.21px_0px_#0000004F] w-[39vw] p-[1.4vw]'>
-        <div className='bg-[#111111] shadow-[15.76px_15.76px_54.63px_0px_#0000004F] rounded-[1.7vw] p-[1.5vw]'>
-          <div className='w-[0.8vw] h-[0.8vw] float-end'>
-            <Image src={info} className='w-full'/>
-          </div>
-          <div className='flex gap-[2vw] justify-center items-end my-[1vw]'>
-            <div>
-              <p className='text-[#494949] font-bebasneue text-[1.7vw]'>DAYS</p>
-              <p className='text-white font-montserrat text-[2.7vw] font-[800]'>{timeLeft.days}</p>
+    <div className='flex justify-center gap-[3.9vw] mt-[1vw] sm:flex-row flex-col'>
+      
+      <div className='bg-[#1B1C1E] sm:rounded-[2.5vw] rounded-[5vw] shadow-[16.21px_16.21px_56.21px_0px_#0000004F] sm:w-[39vw] w-[95%] sm:mx-none mx-auto sm:p-[1.4vw] p-[4.2vw]'>
+        <motion.div 
+          key="1"
+          initial={{ rotateX: 0 }}
+          animate={hoveredCard === 1 ? { rotateX: 180 } : { rotateX: 0 }}
+          transition={{ duration: 0.6 }}
+          className='bg-[#111111] card shadow-[15.76px_15.76px_54.63px_0px_#0000004F] sm:rounded-[1.7vw] rounded-[4.1vw] sm:p-[1.4vw] p-[4vw]' onMouseLeave={() => setHoveredCard(null)}>
+          <div className='card-front' >
+            <div className='sm:w-[0.8vw] sm:h-[0.8vw] w-[2.5vw] h-[2.5vw] float-end' onMouseOver={() => setHoveredCard(1)}>
+              <Image src={info} className='w-full'/>
             </div>
-            <p className='text-white font-montserrat text-[2.7vw] font-[500]'>:</p>
-            <div>
-              <p className='text-[#494949] font-bebasneue text-[1.7vw]'>HRS</p>
-              <p className='text-white font-montserrat text-[2.7vw] font-[800]'>{timeLeft.hours}</p>
-            </div>
-            <p className='text-white font-montserrat text-[2.7vw] font-[500]'>:</p>
-            <div>
-              <p className='text-[#494949] font-bebasneue text-[1.7vw]'>MINS</p>
-              <p className='text-white font-montserrat text-[2.7vw] font-[800]'>{timeLeft.minutes}</p>
-            </div>
-
-          </div>
-
-        </div>
-        <div className='flex items-center gap-[1vw] w-full mt-[1.4vw]'>
-          <div className='bg-[#111111] shadow-[13.13px_13.13px_45.5px_0px_#0000004F] w-full  rounded-[0.9vw] py-[0.7vw] px-[1.2vw] flex justify-between items-center'>
-            <div className='flex items-start gap-[0.4vw]'>
-              <p className='font-bebasneue text-primary text-[1.4vw] '>claimable zilla   </p>
-              <div className='w-[0.65vw] h-[0.65vw] float-end'>
-                <Image src={info} className='w-full'/>
+            <div className='flex sm:gap-[2vw] gap-[4vw] justify-center items-end my-[1vw]'>
+              <div>
+                <p className='text-[#494949] font-bebasneue sm:text-[1.7vw] text-[4.1vw]'>DAYS</p>
+                <p className='text-white font-montserrat sm:text-[2.7vw] text-[6.3vw] font-[800]'>{timeLeft.days==null?"00":timeLeft.days}</p>
               </div>
+              <p className='text-white font-montserrat sm:text-[2.7vw] text-[6.3vw] font-[500]'>:</p>
+              <div>
+                <p className='text-[#494949] font-bebasneue sm:text-[1.7vw] text-[4.1vw]'>HRS</p>
+                <p className='text-white font-montserrat sm:text-[2.7vw] text-[6.3vw] font-[800]'>{timeLeft.hours==null?"00":timeLeft.hours}</p>
+              </div>
+              <p className='text-white font-montserrat sm:text-[2.7vw] text-[6.3vw] font-[500]'>:</p>
+              <div>
+                <p className='text-[#494949] font-bebasneue sm:text-[1.7vw] text-[4.1vw]'>MINS</p>
+                <p className='text-white font-montserrat sm:text-[2.7vw] text-[6.3vw] font-[800]'>{timeLeft.minutes==null?"00":timeLeft.minutes}</p>
+              </div>
+
             </div>
-            <p className='text-[1.5vw] text-white font-montserrat font-[800]'>123,456,789</p>
           </div>
-          <p className='round text-[#262626] bg-primary text-[2vw] px-[1.9vw] rounded-full leading-none pt-[0.7vw] pb-[0.2vw]'>CLAIM</p>
+          <div className="card-back w-[90%] mx-auto h-full" >
+            <p className='font-montserrat text-white font-[800] text-center sm:text-[1.1vw] text-[2.5vw]'>The countdown until your referral rewards are calculated and reset for the next cycle.</p>
+          </div>
+        </motion.div>
+        <div className='flex items-center sm:gap-[1vw] gap-[2.5vw] w-full sm:mt-[1.4vw] mt-[2.6vw]'>
+          <div className='bg-[#111111] shadow-[13.13px_13.13px_45.5px_0px_#0000004F] w-full  sm:rounded-[0.9vw] rounded-[2.6vw] sm:py-[0.8vw] py-[1.9vw] sm:px-[1.8vw] px-[4.8vw] flex justify-between items-center'>
+            <div className='flex items-start gap-[0.4vw]'>
+              <p className='font-bebasneue text-primary sm:sm:text-[1.4vw] text-[4.3vw] text-[3.3vw] '>claimable zilla   </p>
+              {/* <div className='sm:w-[0.65vw] sm:h-[0.65vw] w-[1.9vw] h-[1.9vw] float-end'>
+                <Image src={info} className='w-full'/>
+              </div> */}
+            </div>
+            <p className='sm:sm:text-[1.4vw] text-[4.3vw] text-[3.3vw] text-white font-montserrat font-[800]'>123,456,789</p>
+          </div>
+          <p className='round  anim text-[#262626] bg-primary sm:text-[2vw] text-[4.9vw] sm:px-[1.9vw] px-[5vw] rounded-full leading-none sm:pt-[0.7vw] pt-[1.5vw] pb-[0.2vw]'>CLAIM</p>
         </div>
       </div>
-      <div className='bg-[#1B1C1E] rounded-[2.5vw] shadow-[16.21px_16.21px_56.21px_0px_#0000004F] w-[39vw] p-[1.4vw] flex flex-col gap-[1vw]'>
-        <div className='bg-[#111111] shadow-[23.71px_23.71px_82.18px_0px_#0000004F] rounded-[1vw] w-full py-[1.3vw] px-[3vw] flex justify-between items-center'>
+
+      <div className='bg-[#1B1C1E] sm:rounded-[2.5vw] rounded-[5vw] shadow-[16.21px_16.21px_56.21px_0px_#0000004F] sm:w-[39vw] w-[95%] sm:mx-none mx-auto sm:p-[1.4vw] p-[4.2vw] flex flex-col sm:gap-[1vw] gap-[3vw]'>
+        <div className='bg-[#111111] shadow-[23.71px_23.71px_82.18px_0px_#0000004F] sm:rounded-[1vw]  rounded-[3vw] w-full sm:py-[1.3vw] py-[2.6vw] sm:px-[3vw] px-[7.2vw] flex justify-between items-center'>
           <div className='flex items-start gap-[0.4vw]'>
-              <p className='font-bebasneue text-primary text-[1.4vw] '>claimable zilla   </p>
-              <div className='w-[0.65vw] h-[0.65vw] float-end'>
+              <p className='font-bebasneue text-primary sm:text-[1.4vw] text-[4.3vw] '>referral uses </p>
+              {/* <div className='sm:w-[0.65vw] sm:h-[0.65vw] w-[1.9vw] h-[1.9vw] float-end'>
                 <Image src={info} className='w-full'/>
-              </div>
+              </div> */}
           </div>
-          <p className='text-[1.5vw] text-white font-montserrat font-[800]'>123,456,789</p>
+          <p className='sm:text-[1.5vw] text-[5vw] text-white font-montserrat font-[800]'>123,456,789</p>
         </div>
-        <div className='bg-[#111111] shadow-[23.71px_23.71px_82.18px_0px_#0000004F] rounded-[1vw] w-full py-[1.3vw] px-[3vw] flex justify-between items-center'>
-          <div className='flex items-start gap-[0.4vw]'>
-              <p className='font-bebasneue text-primary text-[1.4vw] '>claimable zilla   </p>
-              <div className='w-[0.65vw] h-[0.65vw] float-end'>
-                <Image src={info} className='w-full'/>
-              </div>
+        <motion.div 
+        key="2"
+        initial={{ rotateX: 0 }}
+        animate={hoveredCard === 2 ? { rotateX: 180 } : { rotateX: 0 }}
+        transition={{ duration: 0.6 }}
+        className='bg-[#111111] card shadow-[23.71px_23.71px_82.18px_0px_#0000004F] sm:rounded-[1vw]  rounded-[3vw] w-full sm:py-[1.3vw] py-[2.6vw]  ' onMouseLeave={()=> setHoveredCard(null)}>
+          <div className="card-front flex justify-between items-center sm:px-[3vw] px-[7.2vw]">
+            <div className='flex items-start sm:gap-[0.6vw] gap-[1.5vw]'>
+                <p className='font-bebasneue text-primary sm:text-[1.4vw] text-[4.3vw] '>ETH GENERATED   </p>
+                <div className='sm:w-[0.65vw] sm:h-[0.65vw] w-[1.9vw] h-[1.9vw] float-end' onMouseOver={()=>{setHoveredCard(2)}}>
+                  <Image src={info} className='w-full'/>
+                </div>
+            </div>
+            <p className='sm:text-[1.5vw] text-[5vw] text-white font-montserrat font-[800]'>123,456,789</p>
           </div>
-          <p className='text-[1.5vw] text-white font-montserrat font-[800]'>123,456,789</p>
-        </div>
-        <div className='bg-[#111111] shadow-[23.71px_23.71px_82.18px_0px_#0000004F] rounded-[1vw] w-full py-[1.3vw] px-[3vw] flex justify-between items-center'>
-          <div className='flex items-start gap-[0.4vw]'>
-              <p className='font-bebasneue text-primary text-[1.4vw] '>claimable zilla   </p>
-              <div className='w-[0.65vw] h-[0.65vw] float-end'>
-                <Image src={info} className='w-full'/>
-              </div>
+          <div className="card-back w-full">
+            <p className='font-montserrat text-white font-[800] text-center sm:text-[0.8vw] text-[1.8vw] w-[80%] mx-auto' >This is the total ETH spent by people using your referral links. Keep sharing and watch it grow!</p>
           </div>
-          <p className='text-[1.5vw] text-white font-montserrat font-[800]'>123,456,789</p>
-        </div>
+        </motion.div>
+        <motion.div 
+        key="3"
+        initial={{ rotateX: 0 }}
+        animate={hoveredCard === 3 ? { rotateX: 180 } : { rotateX: 0 }}
+        transition={{ duration: 0.6 }}
+        className='card bg-[#111111] shadow-[23.71px_23.71px_82.18px_0px_#0000004F] sm:rounded-[1vw]  rounded-[3vw] w-full sm:py-[1.3vw] py-[2.6vw]  ' onMouseLeave={()=> setHoveredCard(null)}>
+          <div className="card-front sm:px-[3vw] px-[7.2vw] flex justify-between items-center">
+            <div className='flex items-start sm:gap-[0.6vw] gap-[1.5vw]'>
+                <p className='font-bebasneue text-primary sm:text-[1.4vw] text-[4.3vw] '>ETH SPENT   </p>
+                <div className='sm:w-[0.65vw] sm:h-[0.65vw] w-[1.9vw] h-[1.9vw] float-end' onMouseOver={()=>{setHoveredCard(3)}}>
+                  <Image src={info} className='w-full'/>
+                </div>
+           </div>
+            <p className='sm:text-[1.5vw] text-[5vw] text-white font-montserrat font-[800]'>123,456,789</p>
+          </div>
+          <div className="card-back w-full">
+            <p className='font-montserrat text-white font-[800] text-center sm:text-[0.8vw] text-[1.8vw] w-[100%]' >This is how much ETH you’ve spent using other users' referral links!</p>
+          </div>
+        </motion.div>
       </div>
     </div>
   )

f2ecdbc025a0f8594b947574734a1085c2a13dd8
vivek sagar <vivek54540@gmail.com>
2025-02-25 20:11:06 +0530

    launch page v5.8

--- src/components/LaunchForm.jsx
+++ src/components/LaunchForm.jsx
@@ -4,87 +4,83 @@ import React, { useState } from 'react'
 import cam from "@/assets/cam.png"
 import Image from 'next/image'
 import LaunchFooter from './LaunchFooter'
+import TickerModel from './TickerModel'
 
 const LaunchForm = () => {  
     const [ticker,setTicker] = useState("")
-  return (
-    <div className='md:pt-[2vw] pt-[8.4vw]'>
+    const [openTic,setOpenTic] =useState(true)
+  
+    return (
+    <>
+    
+    <div className='md:pt-10 pt-[8.4vw]'>
 
       <div>
-        <h1 className='font-montserrat font-[900] text-white md:text-[2vw] text-[4.7vw] text-center'>LAUNCH YOUR <span className='text-primary'>TOKEN</span></h1>
-        {/* <p className='text-white opacity-45 font-[700] font-montserrat md:text-[17px] text-[2.3vw] text-center md:mt-5 mt-1'>No presale, No Team Allocation, Lower Gas</p> */}
-      </div>
-      <div className='flex flex-row md:px-[0.8vw] justify-center items-center w-full'>
+        <h1 className='font-montserrat font-[900] text-white 2xl:text-4xl md:text-3xl text-[4.7vw] text-center'>LAUNCH YOUR <span className='text-primary'>TOKEN</span></h1>
+       </div>
+      <div className='w-full 2xl:mt-10 mt-8'>
 
-      {/* <LaunchFooter/> */}
-
-        <div className='bg-[#222227] font-montserrat md:w-[41vw] w-[88vw] md:p-[3.1vw] p-[4.8vw] md:rounded-[2.5vw] rounded-[3.4vw] shadow-[26px_25px_51.4px_0px_#00000040] mx-auto md:my-[2vw] my-[5.3vw]'>
-          <div className='flex justify-between md:gap-[3vw] gap-[4.1vw]'>
+        <div className='bg-[#222227] font-montserrat 2xl:w-[50rem] md:w-[35rem] w-[88vw] 2xl:p-12 md:p-9 p-[4.8vw] 2xl:rounded-[2.8rem] md:rounded-[2.5rem] rounded-[3.4vw] shadow-[26px_25px_51.4px_0px_#00000040] mx-auto'>
+          <div className='flex justify-between 2xl:gap-14 md:gap-9 gap-[4.1vw]'>
             <div className='w-full'>
               <div>
-                <p className='text-primary md:text-[1vw] text-[2.1vw] font-[800] '>NAME</p>
-                <input type="text" placeholder='ENTER TOKEN NAME' className='border-0 md:h-[2.5vw] h-[6vw] md:md:mt-[0.2vw] mt-[0.8vw] w-full bg-[#38383F] opacity-60 font-[400] font-bebasneue m-0 text-white md:text-[0.8vw] text-[2.3vw] md:px-[0.9vw] px-[2.4vw] py-[5px] md:rounded-[0.6vw] rounded-[0.9vw] shadow-[20px_20px_33.3px_0px_#00000066]'/>
+                <p className='text-primary 2xl:text-lg md:text-sm text-[2.1vw] font-[800] '>NAME</p>
+                <input type="text" placeholder='ENTER TOKEN NAME' className='border-0 2xl:h-[2.8rem] md:h-[2.2rem] h-[6vw] md:md:mt-1 mt-[0.8vw] w-full bg-[#38383F] opacity-60 font-[400] font-bebasneue m-0 text-white 2xl:text-sm md:text-[0.7rem] text-[2.3vw] 2xl:px-4 md:px-3 px-[2.4vw] 2xl:rounded-[0.8rem] md:rounded-[0.6rem] rounded-[0.9vw] shadow-[20px_20px_33.3px_0px_#00000066]'/>
               </div>
               <div>
-                <div className='flex md:mt-[0.8vw] mt-[2vw] items-center md:gap-[0.4vw] gap-[1vw]'>
-                  <p className='text-primary md:text-[1vw] text-[2.1vw] font-[800] '>TICKER</p>
-                  <p className={` ${ticker.length>10?"block":"hidden"} text-[#FF2F61] font-[600] md:text-[0.8vw] text-[1.4vw] `}>*Ticker cannot be more than 10 charachters</p>
+                <div className='flex 2xl:mt-5 md:mt-3 mt-[2vw] items-center 2xl:gap-3 md:gap-2 gap-[1vw]'>
+                  <p className='text-primary  2xl:text-lg md:text-sm text-[2.1vw]  font-[800] '>TICKER</p>
+                  <p className={` ${ticker.length>10?"block":"hidden"} text-[#FF2F61] font-[600] 2xl:text-sm md:text-[0.55rem] text-[1.4vw] `}>*Ticker cannot be more than 10 charachters</p>
                 </div>
                 <input type="text" onChange={(e)=>{
                   setTicker(e.target.value)
-                }} placeholder='ENTER TOKEN TICKER (10 CHARACTERS)' className='border-0 md:h-[2.5vw] h-[6vw] md:md:mt-[0.2vw] mt-[0.8vw] w-full bg-[#38383F] opacity-60 font-[400] font-bebasneue m-0 text-white md:text-[0.8vw] text-[2.3vw] md:px-[0.9vw] px-[2.4vw] py-[5px] md:rounded-[0.6vw] rounded-[0.9vw] shadow-[20px_20px_33.3px_0px_#00000066]'/>
+                }} placeholder='ENTER TOKEN TICKER (10 CHARACTERS)' className='border-0 2xl:h-[2.8rem] md:h-[2.2rem] h-[6vw] md:md:mt-1 mt-[0.8vw] w-full bg-[#38383F] opacity-60 font-[400] font-bebasneue m-0 text-white 2xl:text-sm md:text-[0.7rem] text-[2.3vw] 2xl:px-4 md:px-3 px-[2.4vw] 2xl:rounded-[0.8rem] md:rounded-[0.6rem] rounded-[0.9vw] shadow-[20px_20px_33.3px_0px_#00000066]'/>
               </div>
             </div>
-            <div className='bg-[#38383F] flex flex-col justify-center items-center  md:rounded-[1.4vw] rounded-[3.3vw] md:w-[10.5vw] w-[25vw] md:h-[10.5vw] h-[25vw] md:border-[0.2vw] border-[2px] border-[#A8FF2F] md:p-[2.8vw] p-[7.2vw]'>
-              <div className='md:w-[4.1vw] w-[9.5vw]'>
+            <div className='bg-[#38383F] mt-1 flex flex-col justify-center items-center  2xl:rounded-[1.7rem] md:rounded-[1.3rem] rounded-[3.3vw] 2xl:w-52 md:w-36 w-[25vw] 2xl:h-48 md:h-36 h-[25vw] md:border-[2px] border-[2px] border-[#A8FF2F] md:p-[2.8rem] p-[7.2vw]'>
+              <div className='2xl:w-[4.5rem] md:w-[3rem] w-[9.5vw]'>
                 <Image unoptimized={true} alt="camera" className='w-full' src={cam}/>
               </div>
               <div>
-                <p className='font-bebasneue  font-[400] text-center md:mt-[1.2vw] mt-2 mb-0 md:text-[0.9vw] text-[2.1vw] mx-auto text-[#65656A] '>JPEG/PNG/WEBP/GIF</p>
-                <p className='font-bebasneue  font-[400] text-center md:text-[0.7vw] text-[2.1vw]  m-0 mx-auto text-[#65656A] '>LESS THAN 4MB</p>
+                <p className='font-bebasneue  font-[400] text-center 2xl:mt-5 md:mt-3 mt-2 mb-0 2xl:text-base md:text-sm text-[2.1vw] mx-auto text-[#65656A] '>JPEG/PNG/WEBP/GIF</p>
+                <p className='font-bebasneue  font-[400] text-center 2xl:text-xs md:text-[0.55rem] text-[2.1vw]  m-0 mx-auto text-[#65656A] '>LESS THAN 4MB</p>
               </div>
             </div>
 
           </div>
 
           <div>
-                <p className='text-primary md:text-[1vw] text-[2.1vw] font-[800]'>DESCRIPTION</p>
-                <textarea  placeholder='ENTER TOKEN DESCRIPTION' className='border-0 md:h-[4.5vw] h-[9.7vw] md:mt-[0.2vw] mt-[0.8vw] w-full bg-[#38383F] opacity-60 font-[400] font-bebasneue m-0 text-white md:text-[0.8vw] text-[2.3vw] md:px-[0.9vw] px-[2.4vw] py-[0.5vw] md:rounded-[0.6vw] rounded-[1.4vw] shadow-[20px_20px_33.3px_0px_#00000066]'/>
+                <p className='text-primary 2xl:text-lg md:text-sm text-[2.1vw] font-[800]'>DESCRIPTION</p>
+                <textarea  placeholder='ENTER TOKEN DESCRIPTION' className='border-0 2xl:h-[7.5rem] md:h-[5rem] h-[16vw] md:mt-1 mt-[0.8vw] w-full bg-[#38383F] opacity-60 font-[400] font-bebasneue m-0 text-white 2xl:text-sm md:text-[0.7rem] text-[2.3vw] 2xl:px-4 md:px-3 px-[2.4vw] py-2 2xl:rounded-[0.8rem] md:rounded-[0.6rem] rounded-[1.4vw] shadow-[20px_20px_33.3px_0px_#00000066]'/>
               </div>
-            <div className='flex md:flex-row flex-col md:gap-[2vw] w-full'>
+            <div className='flex md:flex-row flex-col md:gap-8 w-full'>
               <div className='w-full'>
-                <p className='text-primary md:text-[1vw] text-[2.1vw] font-[800] md:mt-[0.8vw] mt-[2vw]'>TWITTER</p>
-                <input type="text" placeholder='OPTIONAL' className='border-0 md:h-[2.5vw] h-[6vw] md:mt-[0.2vw] mt-[0.8vw] w-full bg-[#38383F] opacity-60 font-[400] font-bebasneue m-0 text-white md:text-[0.8vw] text-[2.3vw] md:px-[0.9vw] px-[2.4vw] py-[0.2vw] md:rounded-[0.6vw] rounded-[0.9vw] shadow-[20px_20px_33.3px_0px_#00000066]'/>
+                <p className='text-primary 2xl:text-lg md:text-sm text-[2.1vw] font-[800] 2xl:mt-5 md:mt-3 mt-[2vw]'>TWITTER</p>
+                <input type="text" placeholder='OPTIONAL' className='border-0 2xl:h-[2.8rem] md:h-[2.2rem] h-[6vw] md:md:mt-1 mt-[0.8vw] w-full bg-[#38383F] opacity-60 font-[400] font-bebasneue m-0 text-white 2xl:text-sm md:text-[0.7rem] text-[2.3vw] 2xl:px-4 md:px-3 px-[2.4vw] 2xl:rounded-[0.8rem] md:rounded-[0.6rem] rounded-[0.9vw] shadow-[20px_20px_33.3px_0px_#00000066]'/>
               </div>
               <div className='w-full'>
-                <p className='text-primary md:text-[1vw] text-[2.1vw] font-[800] md:mt-[0.8vw] mt-[2vw]'>TELEGRAM</p>
-                <input type="text" placeholder='OPTIONAL' className='border-0 md:h-[2.5vw] h-[6vw] md:mt-[0.2vw] mt-[0.8vw] w-full bg-[#38383F] opacity-60 font-[400] font-bebasneue m-0 text-white md:text-[0.8vw] text-[2.3vw] md:px-[0.9vw] px-[2.4vw] py-[0.2vw] md:rounded-[0.6vw] rounded-[0.9vw] shadow-[20px_20px_33.3px_0px_#00000066]'/>
+                <p className='text-primary 2xl:text-lg md:text-sm text-[2.1vw] font-[800] 2xl:mt-5 md:mt-3 mt-[2vw]'>TELEGRAM</p>
+                <input type="text" placeholder='OPTIONAL' className='border-0 2xl:h-[2.8rem] md:h-[2.2rem] h-[6vw] md:md:mt-1 mt-[0.8vw] w-full bg-[#38383F] opacity-60 font-[400] font-bebasneue m-0 text-white 2xl:text-sm md:text-[0.7rem] text-[2.3vw] 2xl:px-4 md:px-3 px-[2.4vw] 2xl:rounded-[0.8rem] md:rounded-[0.6rem] rounded-[0.9vw] shadow-[20px_20px_33.3px_0px_#00000066]'/>
               </div>
             </div>
 
             <div>
-                <p className='text-primary md:text-[1vw] text-[2.1vw] font-[800] md:mt-[0.8vw] mt-[2vw]'>WEBSITE</p>
-                <input type="text" placeholder='OPTIONAL' className='border-0 md:h-[2.5vw] h-[6vw] md:mt-[0.2vw] mt-[0.8vw] w-full bg-[#38383F] opacity-60 font-[400] font-bebasneue m-0 text-white md:text-[0.8vw] text-[2.3vw] md:px-[0.9vw] px-[2.4vw] py-[0.2vw] md:rounded-[0.6vw] rounded-[0.9vw] shadow-[20px_20px_33.3px_0px_#00000066]'/>
+                <p className='text-primary 2xl:text-lg md:text-sm text-[2.1vw] font-[800] 2xl:mt-5 md:mt-3 mt-[2vw]'>WEBSITE</p>
+                <input type="text" placeholder='OPTIONAL' className='border-0 2xl:h-[2.8rem] md:h-[2.2rem] h-[6vw] md:md:mt-1 mt-[0.8vw] w-full bg-[#38383F] opacity-60 font-[400] font-bebasneue m-0 text-white 2xl:text-sm md:text-[0.7rem] text-[2.3vw] 2xl:px-4 md:px-3 px-[2.4vw] 2xl:rounded-[0.8rem] md:rounded-[0.6rem] rounded-[0.9vw] shadow-[20px_20px_33.3px_0px_#00000066]'/>
               </div>
-              <div>
-                <p className='text-primary md:text-[1vw] text-[2.1vw] font-[800] md:mt-[0.8vw] mt-[2vw]'>INITIAL BUY</p>
-
-                <div className='flex justify-center items-center md:h-[2.5vw] h-[6vw] md:mt-[0.2vw] mt-[0.8vw] w-full bg-[#38383F] font-[400] font-bebasneue m-0 text-white md:text-[1vw] text-[2.1vw] md:px-[0.8vw] px-[2.4vw] md:py-[0.5vw] py-[0.9vw] md:rounded-[0.6vw] rounded-[0.9vw] shadow-[20px_20px_33.3px_0px_#00000066]'>
-                  <input type="text" placeholder='ENTER AMOUNT' className='border-0 w-full bg-[#38383F] opacity-60 font-[400] font-bebasneue text-white md:text-[0.8vw] text-[2.3vw]'/>
-                    <span className='md:w-[0.3vw] w-[0.5vw] rounded-full  bg-white opacity-20 h-full'></span>
-                  <p className='font-bebasneue font-[400] md:ms-[0.7vw] ms-[2vw] md:w-[10.4vw] w-[18vw] md:text-[1vw] text-[2.1vw] text-white  '>USER BALANCE : <span className='text-primary'>0 ETH</span></p>
-                </div>
               
+              <div className='w-full flex justify-center 2xl:mt-8 md:mt-6 mt-[7.2vw]'>
+                <button onClick={()=>setOpenTic(true)} className='anim border-0 bg-primary shadow-[0px_4px_4px_0px_#00000040] text-[#38383F] mx-auto round font-[500] 2xl:text-5xl md:text-3xl text-[5.8vw] 2xl:w-[12rem] md:w-[8.5rem] w-[28vw] md:leading-none leading-[6vw] 2xl:pt-[0.6rem] md:pt-[0.4rem] pt-[1.2vw] rounded-full'>LAUNCH</button>
               </div>
-              <div className='w-full flex justify-center md:mt-[2.6vw] mt-[7.2vw]'>
-                <button className='anim border-0 bg-primary shadow-[0px_4px_4px_0px_#00000040] text-[#38383F] mx-auto round font-[500] md:text-[2.3vw] text-[5.8vw] md:w-[11.6vw] w-[28vw] md:leading-[2.6vw] leading-[6vw] md:pt-[0.6vw] pt-[1.2vw] rounded-full'>LAUNCH</button>
-              </div>
-              <p className="md:text-[1vw] text-[2vw] font-montserrat font-[800] text-white text-center md:mt-[2.4vw] mt-[4vw]">Receive <span className='text-primary'>0.1 ETH</span> when your <span className='text-primary'>token completes its bonding curve</span></p>
+              <p className="2xl:text-lg md:text-xs text-[2.8vw] font-montserrat font-[800] text-white text-center 2xl:mt-6 md:mt-4 mt-[4vw]">Receive <span className='text-primary'>0.1 ETH</span> when your <span className='text-primary'>token completes its bonding curve</span></p>
         </div>
 
       </div>
       
       </div>
+    
+    <TickerModel openTic={openTic} setOpenTic={setOpenTic}/>
+    </>
   )
 }
 
---
+++ src/components/TickerModel.jsx
@@ -0,0 +1,94 @@
+import { motion } from "framer-motion";
+import React, { useState } from 'react';
+import cross from "@/assets/cross.png";
+import eth from "@/assets/eth.png";
+import pif from "@/assets/pif.png";
+import cam from "@/assets/cam.png"
+import Image from "next/image";
+const TickerModel = ({openTic,setOpenTic}) => {
+    const [ticker,setTicker] = useState(false)
+    const handleClose = (e) => {
+        if (e.target.id === "background") {
+          setOpenTic(false);
+        }
+    }
+
+      
+  return (
+    <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: open ? 1 : 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          className={`${
+            openTic ? "flex " : "hidden"
+          } bg-[#00000080] fixed z-20  justify-center items-center w-full h-screen  top-0`}
+          onClick={(e) => handleClose(e)}
+          id="background">
+            
+            <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: openTic ? 1 : 0 }}
+                    exit={{ opacity: 0, y: -50 }}
+                    transition={{ duration: 0.3 }}
+                    id="box"
+                    className={`${
+                        openTic ? "block" : "hidden"
+                    } bg-[#1B1C1E] 2xl:w-[50rem] lg:w-[39rem] md:w-[40rem] w-[98vw] h-min md:rounded-[1.7rem] rounded-[2.5vw]`}>
+
+                    <div className="w-full flex justify-end md:pt-3 pt-3 md:pe-3 pe-3">
+                        <div className="2xl:w-9 md:w-7 w-[4.5vw] cursor-pointer anim">
+                
+                            <Image
+                                src={cross}
+                                className=" w-full "
+                                onClick={() => setOpenTic(false)}
+                                alt=""
+                            />
+                        </div>
+                    </div>
+
+                    <div className="2xl:px-16 md:px-10 px-[7vw]">
+                        
+                        <p className="2xl:text-3xl md:text-2xl text-[3.6vw] text-white font-[700] font-montserrat text-center" >Choose How Many <span className="text-primary">Ticker</span>  You Want to Buy</p>
+                        <p className="md:mt-6 mt-[4.5vw] font-montserrat leading-tight text-center font-[600] text-white border-s-primary md:border-s-[6px] border-s-[3px] md:py-1  2xl:text-lg md:text-sm text-2xl text-[2.3vw]"><span className="text-primary">Disclaimer</span > : <span className="opacity-40"> Its optional, but buying a small amount of coins can help</span> protect your coin from snipers</p>
+                        
+                        <div className="w-full flex justify-end md:my-5 my-[2.9vw]">
+                            <p onClick={()=>setTicker(!ticker)} className="bg-primary text-[#222227] 2xl:text-base md:text-sm text-[2.3vw] font-bebasneue 2xl:w-32 md:w-28 w-[18.9vw] py-0.5 text-center rounded-full">Switch To {ticker?"ETH":"ticker"}</p>
+                        </div>
+                        <div className="bg-[#111111] shadow-2xl md:px-8 px-[5.3vw] w-full md:py-6 py-[2.6vw] rounded-xl">
+                            <div className="flex justify-between items-center">
+                                <input type="number" className="w-full bg-transparent text-white font-montserrat font-[700] 2xl:text-5xl md:text-4xl text-[5vw] border-0 focus:outline-none" placeholder="0" />
+                                <div className="2xl:w-16 md:w-10 w-[5.8vw]">
+                                    {ticker? 
+                                    <Image src={pif} alt={""}/>
+        
+                                    :
+                                    <Image src={eth} alt={""}/>
+                                    }
+                                </div>
+                            </div>
+                            <div>
+                            <div className="flex items-center justify-between 2xl:mt-2 md:mt-1">
+                                <p className="text-[#4E4F54] 2xl:text-base md:text-sm text-[2.5vw] font-bebasneue">{ticker?"COST : [x] ETH":"you receive : [x] Ticker"}</p>
+                                <p className="text-[#4E4F54] 2xl:text-base md:text-sm text-[2.5vw] font-bebasneue">0.0000 ETH</p>
+
+                            </div>
+                        </div>
+
+                        </div>
+
+                        
+                    </div>
+                    <div className="">
+                        <p className="text-[#1B1C1E] anim bg-primary round 2xl:text-7xl md:text-5xl text-[8vw] leading-none md:pt-3 pt-[1.4vw] text-center mx-auto 2xl:w-[25rem] md:w-[17rem] w-[43vw] 2xl:my-12 my-7 rounded-full">create coin</p>
+                    </div>
+
+            </motion.div>
+            
+      
+    </motion.div>
+  )
+}
+
+export default TickerModel

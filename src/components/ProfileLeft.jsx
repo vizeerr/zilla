92ee2e5949c2812fde7a6873410762d44466bf94
vivek sagar <vivek54540@gmail.com>
2025-03-04 15:15:17 +0530

    profile page v7.0

--- src/components/ProfileLeft.jsx
+++ src/components/ProfileLeft.jsx
@@ -13,7 +13,7 @@ import stakearr from '@/assets/stakearr.png'
 import Image from 'next/image'
 import { motion } from 'framer-motion';
 const ProfileLeft = () => {
-    const[editProfile,setEditProfile]=useState(true)
+    const[editProfile,setEditProfile]=useState(false)
     const[profileBio,setProfileBio]=useState('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ')
   return (
     <div className='2xl:w-[55%] xl:w-[45%] w-full h-full '>

e02d220b5e535a0b1c46b670deefa7435016f602
vivek sagar <vivek54540@gmail.com>
2025-03-12 20:02:39 +0530

    navbar v3.3

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -26,53 +26,53 @@ const TopNavbar = () => {
     
     <div className="sticky top-0  z-20">
       <div className="bg-primary flex items-center justify-between 2xl:py-3 lg:py-2 py-[1vw]">
-        <div className="lg:ps-6 ps-[2.1vw] w-[20vw] lg:w-44 md:w-32">
+        <div className="2xl:ps-6 lg:ps-4 ps-[2.1vw] w-[20vw] 2xl:w-44 lg:w-36 md:w-32">
           <Link href={"/"}>
             <Image className="w-full"  src={logo} alt="" />
           </Link>
         </div>
-        <div className="flex items-center md:gap-3 gap-[1vw] md:pe-4 pe-[2.5vw]">
-          <div className="md:flex hidden items-center md:gap-3 gap-[1.2vw]">
-            <Link href={"/launch"} className={`${pathname == "/launch" ? 'bg-white text-[#262626]' :"bg-sec text-white"}  lg:px-6 md:px-5 anim rounded-full font-bebasneue 2xl:text-lg lg:text-base md:text-sm   pt-0.5  `}>
+        <div className="flex items-center 2xl:gap-3 md:gap-2 gap-[1vw] md:pe-4 pe-[2.5vw]">
+          <div className="md:flex hidden items-center 2xl:gap-3 md:gap-2  gap-[1.2vw]">
+            <Link href={"/launch"} className={`${pathname == "/launch" ? 'bg-white text-[#262626]' :"bg-sec text-white"}  2xl:px-6  lg:px-4 md:px-4 anim rounded-full font-bebasneue 2xl:text-lg lg:text-sm md:text-sm   pt-0.5  `}>
             <button href={"/launch"} >LAUNCH</button>
             </Link>
-            <Link href={"/zilla-token"} className={`${pathname == "/zilla-token" ? 'bg-white text-[#262626]' :"bg-sec text-white"}  lg:px-6 md:px-5 anim rounded-full font-bebasneue 2xl:text-lg lg:text-base md:text-sm  pt-0.5  `} >
+            <Link href={"/zilla-token"} className={`${pathname == "/zilla-token" ? 'bg-white text-[#262626]' :"bg-sec text-white"} 2xl:px-6  lg:px-4 md:px-4 anim rounded-full font-bebasneue 2xl:text-lg lg:text-sm md:text-sm   pt-0.5   `} >
             <button >$ZILLA</button>
             </Link>
             
-            <Link href={"/stake"} className={`${pathname == "/stake" ? 'bg-white text-[#262626]' :"bg-sec text-white"}  lg:px-6 md:px-5 anim rounded-full font-bebasneue 2xl:text-lg lg:text-base md:text-sm  pt-0.5  `}>
+            <Link href={"/stake"} className={`${pathname == "/stake" ? 'bg-white text-[#262626]' :"bg-sec text-white"} 2xl:px-6  lg:px-4 md:px-4 anim rounded-full font-bebasneue 2xl:text-lg lg:text-sm md:text-sm   pt-0.5   `}>
             <button>STAKE</button>
             </Link>
-            <Link href={"/referral"} className={`${pathname == "/referral" ? 'bg-white text-[#262626]' :"bg-sec text-white"}  lg:px-6 md:px-5 anim rounded-full font-bebasneue 2xl:text-lg lg:text-base md:text-sm  pt-0.5  `}>
+            <Link href={"/referral"} className={`${pathname == "/referral" ? 'bg-white text-[#262626]' :"bg-sec text-white"} 2xl:px-6  lg:px-4 md:px-4 anim rounded-full font-bebasneue 2xl:text-lg lg:text-sm md:text-sm   pt-0.5   `}>
             <button>REFERRAL</button>
             </Link>
           </div>
 
-          <div className="lg:w-8 md:w-6 anim lg:h-8 lg:p-2 md:p-1.5 md:h-6 w-[4vw] h-[4vw] bg-sec flex justify-center items-center rounded-full p-[1vw]">
+          <div className="2xl:w-8 lg:w-6 md:w-4 anim 2xl:h-8 2xl:p-2 lg:p-1.5 md:p-1 lg:h-6 md:h-4 w-[4vw] h-[4vw] bg-sec flex justify-center items-center rounded-full p-[1vw]">
             <Link href ={"/"} >
               <Image src={xlogo} alt="" />
             </Link>
           </div>
-          <div className="lg:w-8 md:w-6 anim lg:h-8 lg:p-2 md:p-1.5 md:h-6 w-[4vw] h-[4vw] bg-sec flex justify-center items-center rounded-full p-[1vw]">
+          <div className="2xl:w-8 lg:w-6 md:w-4 anim 2xl:h-8 2xl:p-2 lg:p-1.5 md:p-1 lg:h-6 md:h-4 w-[4vw] h-[4vw] bg-sec flex justify-center items-center rounded-full p-[1vw]">
             <Link href={""} className="me-[0.02rem]">
               <Image src={tlogo}  alt="" />
             </Link>
           </div>
-          <div className="lg:w-8 md:w-6 anim lg:h-8 lg:p-2 md:p-1.5 md:h-6 w-[4vw] h-[4vw] bg-sec flex justify-center items-center rounded-full p-[1vw]">
+          <div className="2xl:w-8 lg:w-6 md:w-4 anim 2xl:h-8 2xl:p-2 lg:p-1.5 md:p-1 lg:h-6 md:h-4 w-[4vw] h-[4vw] bg-sec flex justify-center items-center rounded-full p-[1vw]">
             <Link href ={"/"} >
               <Image src={dlogo} alt="" />
             </Link>
           </div>
-          <div className="lg:w-8 md:w-6 anim lg:h-8 lg:p-2 md:p-1.5 md:h-6 w-[4vw] h-[4vw] bg-sec flex justify-center items-center rounded-full p-[1vw]">
+          <div className="2xl:w-8 lg:w-6 md:w-4 anim 2xl:h-8 2xl:p-2 lg:p-1.5 md:p-1 lg:h-6 md:h-4 w-[4vw] h-[4vw] bg-sec flex justify-center items-center rounded-full p-[1vw]">
             <Link href ={"/"} >
               <Image src={glogo} alt="" />
             </Link>
           </div>
         {!connect?
-          <button onClick={()=>setConnect(true)} className="xl:border-[3px] md:border-[2px] border-[1px] anim border-sec rounded-full font-montserrat lg:py-1.5  md:px-4   font-[800] bg-white anim text-tertiary lg:text-xs md:text-[0.6rem]  text-[1.6vw] px-[1.4vw] pt-[0.6vw] pb-[0.4vw] ">CONNECT WALLET</button>
-        : <div className="cursor-pointer relative flex items-center lg:gap-4 md:gap-3 gap-3 text-white bg-[#262626] xl:border-[3px] md:border-[2px] border-[1px]  border-sec rounded-full font-montserrat lg:py-1.5 md:py-1 lg:px-7 md:px-[1.9rem] font-[800] px-[2.7vw] py-[0.5vw]">
-            <p  onClick = {()=>setConnMenu(!connMenu)} className="lg:text-xs md:text-[0.6rem]  text-[1.6vw]">0x45...67fr</p>
-            <div onClick = {()=>setConnMenu(!connMenu)} className={`${connMenu? "rotate-180":"rotate-0"} lg:w-3 md:w-2 w-1.5`}>
+          <button onClick={()=>setConnect(true)} className="xl:border-[3px] md:border-[1px] border-[1px] anim border-sec rounded-full font-montserrat 2xl:py-1.5 lg:py-1  2xl:px-4 md:px-3   font-[800] bg-white anim text-tertiary 2xl:text-xs  lg:text-[0.6rem] md:text-[0.5rem]  text-[1.6vw] px-[1.4vw] pt-[0.6vw] pb-[0.4vw] ">CONNECT WALLET</button>
+        : <div className="cursor-pointer relative flex items-center 2xl:gap-4 md:gap-3 gap-3 text-white bg-[#262626] xl:border-[3px] md:border-[2px] border-[1px]  border-sec rounded-full font-montserrat 2xl:py-1.5 md:py-1 2xl:px-7 md:px-6 font-[800] px-[2.7vw] py-[0.5vw]">
+            <p  onClick = {()=>setConnMenu(!connMenu)} className="2xl:text-xs lg:text-[0.6rem]  text-[1.6vw]">0x45...67fr</p>
+            <div onClick = {()=>setConnMenu(!connMenu)} className={`${connMenu? "rotate-180":"rotate-0"} 2xl:w-3 md:w-2 w-1.5`}>
               <Image src={poly} alt="" className="w-full" />
             </div>
             {connMenu &&

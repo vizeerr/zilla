686a4ba577f9a400bb12f9c7049d06c28f9e5f47
vivek sagar <vivek54540@gmail.com>
2025-03-16 14:49:13 +0530

    homepage v5.1

--- src/components/home/HeaderProfile.jsx
+++ src/components/home/HeaderProfile.jsx
@@ -1,3 +1,4 @@
+
 "use client"
 import React, { useCallback, useEffect, useState } from 'react'
 import pif from "@/assets/pif.png"
@@ -13,7 +14,7 @@ import { formatTime, shortenString } from '@/helpers/helpers'
 import { useRouter } from 'next/navigation'
 
 const HeaderProfile = () => {
-      const [tokenData,setTokenData] = useState({});
+    const [tokenData,setTokenData] = useState({});
     
       const router = useRouter();
         
@@ -33,7 +34,7 @@ const HeaderProfile = () => {
         }
 
   return (
-        <div role='button' onClick={redirect} className="2xl:w-[24rem] lg:w-[18rem] md:w-[15rem] w-[72.5vw] lg:rounded-[1rem] md:rounded-[0.8rem] rounded-[4.4vw] relative bg-[#2E2F37] shadow-[7.41px_8.65px_11.79px_0px_#00000040] mx-auto  ">
+    <div role='button' onClick={redirect} className="2xl:w-[24rem] lg:w-[18rem] md:w-[15rem] w-[72.5vw] lg:rounded-[1rem] md:rounded-[0.8rem] rounded-[4.4vw] relative bg-[#2E2F37] shadow-[7.41px_8.65px_11.79px_0px_#00000040] mx-auto  ">
             <div className="md:w-14 w-[10.2vw] md:h-14 h-[10.2vw] absolute md:top-[-1.5rem] top-[-5vw] md:end-[-1.5rem] end-[-5vw]">
                 <Image src={pif} className="w-full" alt=""/>
             </div>
@@ -55,14 +56,14 @@ const HeaderProfile = () => {
                         <Image src={tlogo} className="w-full" alt=""/>
                     </Link>
                 </div>
-                <div className="md:mt-2.5 mt-[1.8vw] flex items-center lg:gap-3.5 md:gap-2 gap-[3vw] 2xl:px-4 md:px-2 px-[3.3vw]">
-                    <div className="2xl:w-24 md:w-16 w-[18vw] 2xl:h-24 lg:h-[4rem] md:h-16 h-[18vw] lg:rounded-[0.6rem] md:rounded-[0.4rem] rounded-[2.1vw] overflow-hidden">
+                <div className="md:mt-2.5 mt-[1.8vw] flex items-center 2xl:gap-3.5 lg:gap-2.5 md:gap-2 gap-[3vw] 2xl:px-4 md:px-2.5 px-[3.3vw]">
+                    <div className="2xl:w-32 lg:w-24 md:w-20 w-[27vw] 2xl:h-24 lg:h-[4.3rem] md:h-14 h-[18vw] 2xl:rounded-[0.6rem] lg:rounded-[0.5rem] md:rounded-[0.4rem] rounded-[2.1vw] overflow-hidden">
                         <Image src={pifico} className="w-full" alt=""/>
                     </div>
-                    <div>
+                    <div className='w-full'>
                         <p className="font-bebasneue font-[400] 2xl:text-base lg:text-xs md:text-xs text-[2.8vw] text-nowrap text-white leading-none">{tokenData.name} <span className="text-primary font-montserrat font-[800] 2xl:text-[0.9rem] lg:text-[0.7rem] md:text-[0.6rem] text-[2.4vw]">({tokenData.symbol})</span></p>
                         <p className="font-[800] font-montserrat text-[#6D6D73] 2xl:text-[0.6rem] md:text-[0.5rem] text-[1.9vw] leading-none lg:mt-[0.3rem] md:mt-[0.2rem] mt-[0.8vw]">Created by {shortenString(tokenData.creator || "",4)}</p>
-                        <div className="bg-[#1B1B21] shadow-[5.2px_5.2px_11.96px_0px_#000000C9] lg:py-2 md:py-1.5 py-[0.8vw] w-full text-center md:rounded-[0.6rem] rounded-[2vw] lg:mt-2.5 md:mt-2 mt-[1.6vw]">
+                        <div className="bg-[#1B1B21] shadow-[5.2px_5.2px_11.96px_0px_#000000C9] lg:py-2 md:py-1.5 py-[0.8vw] w-[90%] text-center md:rounded-[0.6rem] rounded-[2vw] lg:mt-2.5 md:mt-2 mt-[1.6vw]">
                             <p className="text-primary 2xl:text-xs font-montserrat lg:text-[0.6rem] md:text-[0.5rem] text-[2.2vw] font-[800] md:mt-0 leading-none mt-[0.45vw]">MARKET CAP : 19.99K</p>
                         </div>
                     </div>                    
